808277749c355eb6f8b22b0980a04bf812f3f23c
calm0406 <calm0406@163.com>
2022-09-25 14:11:10 +0800

    按需加载

---
+++ src/view/User.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@mui/material';
+export default function User() {
+    return (<div>
+        <h2>User</h2>
+        <Button variant="contained">
+            Button
+        </Button>
+    </div>);
+}

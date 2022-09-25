d9961d3b9e15a9522b8f4af24bcfd6ee612cf4f8
calm0406 <calm0406@163.com>
2022-09-25 11:56:28 +0800

    init

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,5 +1,8 @@
+import Button from '@mui/material/Button';
+
 export default function Home() {
     return (<div>
         <h2>Home</h2>
+        <Button variant="contained">Hello World</Button>
     </div>);
 }

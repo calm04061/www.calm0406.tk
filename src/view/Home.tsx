6c323c37016e7188d4ccb0d89e87446228681ba6
Calm0406 <calm0406@163.com>
2023-12-01 21:43:09 +0800

    a

--- src/view/Home.tsx
+++ src/view/Home.tsx
@@ -1,9 +1,13 @@
 import { Button } from '@mui/material';
+// import { Scene } from 'three';
+
 export default function Home() {
+    // const scene = new Scene();
+
     return (<div>
         <h2>Home</h2>
         <Button variant="contained">
-            Button
+
         </Button>
     </div>);
 }
